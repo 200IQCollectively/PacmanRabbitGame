ff8fc4b8a2ca77e0807252ec1296de22a509799d
Sean Nichols <w9107026@live.tees.ac.uk>
2023-02-13 11:56:14 +0000

    right so you need to observe the objects added to a list for it to work???

--- PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
+++ PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
@@ -8,7 +8,10 @@ public class PacmanMazeGen : MonoBehaviour
     [Range(10, 50)] public int width = 28;
     [Range(10, 50)] public int height = 31;
 
-    private List<GameObject> wallList;
+    public List<GameObject> wallList;
+    public List<GameObject> mazeList;
+
+    public GameObject score;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,14 +21,36 @@ public class PacmanMazeGen : MonoBehaviour
 
     private void GenerateMazeLayout()
     {
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < width + 1; x++)
         {
-            for (int z = 0; z < height; z++)
+            for (int z = 0; z < height + 1; z++)
             {
                 var mazeWall = Instantiate(wall, new Vector3(x, 0, z), Quaternion.identity);
                 mazeWall.name = "Wall[" + x + ", " + z + "]";
                 mazeWall.transform.SetParent(gameObject.transform);
+
+                if(x == 0 || x == width || z == 0 || z == height)
+                {
+                    wallList.Add(mazeWall);
+                }
+
+                else
+                {
+                    mazeList.Add(mazeWall);
+                }
             }
+        }
+
+        CarveMaze();
+    }
+
+    private void CarveMaze()
+    {
+        foreach(GameObject wall in mazeList)
+        {
+            var scoreObj = Instantiate(score, new Vector3(wall.transform.position.x, 1, wall.transform.position.z), Quaternion.identity);
+            scoreObj.transform.SetParent(gameObject.transform);
+            Destroy(wall);
         }
     }
 }
