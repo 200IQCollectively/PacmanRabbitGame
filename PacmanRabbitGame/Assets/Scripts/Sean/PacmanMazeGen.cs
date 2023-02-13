6a3180b1b202d66a5276b340f516d6cf777a38e7
Sean Nichols <w9107026@live.tees.ac.uk>
2023-02-13 14:59:16 +0000

    not entirely sure how to get it to work properly

--- PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
+++ PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PacmanMazeGen : MonoBehaviour
 {
     public GameObject wall;
+
     [Range(10, 50)] public int width = 28;
     [Range(10, 50)] public int height = 31;
 
-    public List<GameObject> wallList;
-    public List<GameObject> mazeList;
+    public List<Node> nodes;
+    public List<Node> wallList;
+    public List<Node> mazeList;
+    public List<GameObject> mazeWallList;
 
     public GameObject score;
 
@@ -25,32 +28,41 @@ public class PacmanMazeGen : MonoBehaviour
         {
             for (int z = 0; z < height + 1; z++)
             {
-                var mazeWall = Instantiate(wall, new Vector3(x, 0, z), Quaternion.identity);
-                mazeWall.name = "Wall[" + x + ", " + z + "]";
-                mazeWall.transform.SetParent(gameObject.transform);
+                int random = Random.Range(0, 2);
 
-                if(x == 0 || x == width || z == 0 || z == height)
+                if (x == 0 || x == width || z == 0 || z == height)
                 {
-                    wallList.Add(mazeWall);
+                    Node node = new Node(x, z, true);
+                    nodes.Add(node);
+                    var wallObj = Instantiate(wall, new Vector3(x, 0, z), Quaternion.identity);
+                    wallObj.name = "Node[" + x + ", " + z + "]";
+                    wallObj.transform.SetParent(gameObject.transform);
+                    wallList.Add(node);
                 }
 
                 else
                 {
-                    mazeList.Add(mazeWall);
-                }
+                    if(random == 1)
+                    {
+                        Node node = new Node(x, z, true);
+                        nodes.Add(node);
+                        var wallObj = Instantiate(wall, new Vector3(x, 0, z), Quaternion.identity);
+                        wallObj.name = "Node[" + x + ", " + z + "]";
+                        wallObj.transform.SetParent(gameObject.transform);
+                        mazeList.Add(node);
+                        mazeWallList.Add(wallObj);
+                    }
+
+                    else
+                    {
+                        Node node = new Node(x, z, false);
+                        nodes.Add(node);
+                        var scoreObj = Instantiate(score, new Vector3(x, 1.5f, z), Quaternion.identity);
+                        scoreObj.name = "Node[" + x + ", " + z + "]";
+                        scoreObj.transform.SetParent(gameObject.transform);
+                    }            
+                }       
             }
-        }
-
-        CarveMaze();
-    }
-
-    private void CarveMaze()
-    {
-        foreach(GameObject wall in mazeList)
-        {
-            var scoreObj = Instantiate(score, new Vector3(wall.transform.position.x, 1, wall.transform.position.z), Quaternion.identity);
-            scoreObj.transform.SetParent(gameObject.transform);
-            Destroy(wall);
         }
     }
 }
