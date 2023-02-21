5a9dd30c4fa480625d0d500e9a3dec8d1ccf68e3
tim7882 <32070347+tim7882@users.noreply.github.com>
2023-02-21 14:30:57 +0000

    ai in new level

---
+++ PacmanRabbitGame/Assets/tim's AI/AiLocomotion.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(AgentLinkMover))]
+public class AiLocomotion : MonoBehaviour
+{
+    public Transform Target;
+    public float UpdateSpeed = 0.1f; // how often to calculate path based on targets position.
+    private NavMeshAgent Navagent;
+    [SerializeField]
+    private Animator animator;
+   
+
+   
+
+
+
+    private void Awake()
+    {
+        Navagent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+             
+
+    }
+
+ 
+
+
+    private void Start()
+    {
+        StartCoroutine(FollowTarget());
+
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Move", Navagent.velocity.magnitude);
+
+
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateSpeed);
+
+        while(enabled)
+        {
+            Navagent.SetDestination(Target.transform.position);
+
+            yield return Wait;
+
+        }
+    }
+
+}
