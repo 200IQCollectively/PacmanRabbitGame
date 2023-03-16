0bda69f8d8f360cd60ed4defe4502e9f3f4103d5
Sean Nichols <w9107026@live.tees.ac.uk>
2023-03-16 15:44:20 +0000

    added vertical shapes, just need to loop through all shapes

--- PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
+++ PacmanRabbitGame/Assets/Scripts/Sean/PacmanMazeGen.cs
@@ -86,15 +86,16 @@ public class PacmanMazeGen : MonoBehaviour
             for(int z = 0; z < size.GetLength(1) + 1; z++)
             {
                 //Spawn Obstacles
-                int random = Random.Range(0, 4);
+                int random = Random.Range(0, 12);
 
                 switch (random)
                 {
+                    //Horizontal Shapes
                     case 0:
 
-                        if(!Physics.CheckBox(new Vector3(x + 0.5f, 1, z + 1.5f), new Vector3(2.5f, 1.5f, 3.5f), Quaternion.identity, layer))
+                        if (!Physics.CheckBox(new Vector3(x + 1.5f, 1, z - 0.5f), new Vector3(3f, 1.5f, 2f), Quaternion.identity, layer))
                         {
-                            var wallObj = Instantiate(twoByFour, new Vector3(x, 0.5f, z), Quaternion.identity);
+                            var wallObj = Instantiate(twoByFour, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 90f, 0));
                             wallObj.name = "2x4" + "[" + x + ", " + z + "]";
                             wallObj.transform.SetParent(walls.transform);
                         }
@@ -112,7 +113,32 @@ public class PacmanMazeGen : MonoBehaviour
 
                         break;
 
+                    //Vertical Shapes
                     case 2:
+
+                        if (!Physics.CheckBox(new Vector3(x + 0.5f, 1, z + 1.5f), new Vector3(2f, 1.5f, 3f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(twoByFour, new Vector3(x, 0.5f, z), Quaternion.identity);
+                            wallObj.name = "2x4" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 3:
+
+                        if (!Physics.CheckBox(new Vector3(x, 1, z - 1f), new Vector3(1.5f, 1.5f, 3f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(threeByOne, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 90f, 0));
+                            wallObj.name = "v3x1" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+
+                    //L Shapes - Horizontal + Up, Horizontal + Down, Vertical + Left, Vertical + Right
+                    case 4:
 
                         if (!Physics.CheckBox(new Vector3(x + 1f, 1, z + 0.5f), new Vector3(2.5f, 1.5f, 2f), Quaternion.identity, layer))
                         {
@@ -123,11 +149,78 @@ public class PacmanMazeGen : MonoBehaviour
 
                         break;
 
-                    case 3:
+                    case 5:
 
-                        if (!Physics.CheckBox(new Vector3(x, 1, z + 1.5f), new Vector3(4.5f, 1.5f, 3f), Quaternion.identity, layer))
+                        if (!Physics.CheckBox(new Vector3(x - 1f, 1, z - 0.5f), new Vector3(2.5f, 1.5f, 2f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(lWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 180f, 0));
+                            wallObj.name = "lWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 6:
+
+                        if (!Physics.CheckBox(new Vector3(x - 0.5f, 1, z + 1f), new Vector3(2.5f, 1.5f, 2.5f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(lWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 270f, 0));
+                            wallObj.name = "lWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 7:
+
+                        if (!Physics.CheckBox(new Vector3(x + 0.5f, 1, z - 1f), new Vector3(2.5f, 1.5f, 2.5f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(lWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 90f, 0));
+                            wallObj.name = "lWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    //T Shapes - Horizontal + Up, Horizontal + Down, Vertical + Left, Vertical + Right
+                    case 8:
+
+                        if (!Physics.CheckBox(new Vector3(x, 1, z + 1f), new Vector3(3.5f, 1.5f, 3f), Quaternion.identity, layer))
                         {
                             var wallObj = Instantiate(tWall, new Vector3(x, 0.5f, z), Quaternion.identity);
+                            wallObj.name = "tWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 9:
+
+                        if (!Physics.CheckBox(new Vector3(x, 1, z - 1f), new Vector3(3.5f, 1.5f, 3f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(tWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 180f, 0));
+                            wallObj.name = "tWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 10:
+
+                        if (!Physics.CheckBox(new Vector3(x - 1f, 1, z), new Vector3(3f, 1.5f, 4f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(tWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 270f, 0));
+                            wallObj.name = "tWall" + "[" + x + ", " + z + "]";
+                            wallObj.transform.SetParent(walls.transform);
+                        }
+
+                        break;
+
+                    case 11:
+
+                        if (!Physics.CheckBox(new Vector3(x + 1f, 1, z), new Vector3(3f, 1.5f, 4f), Quaternion.identity, layer))
+                        {
+                            var wallObj = Instantiate(tWall, new Vector3(x, 0.5f, z), Quaternion.Euler(0, 90f, 0));
                             wallObj.name = "tWall" + "[" + x + ", " + z + "]";
                             wallObj.transform.SetParent(walls.transform);
                         }
